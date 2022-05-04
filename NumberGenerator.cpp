9095f4a4e295a3c9c3f8d3a3671fcf714c1c74b0
Armaan Gill <agill10@binghamton.edu>
2022-05-03 21:37:57 -0400

    added sorted and reverse to set generator

--- NumberGenerator.cpp
+++ NumberGenerator.cpp
@@ -5,13 +5,24 @@ NumGen::NumGen(string alg1){
 }
 
 vector<int> NumGen::make_data(int size, string type){
-    if(type == "heap"){
-        vector<int> data;
+    vector<int> data;
+    if(type == "random"){
+        //vector<int> data;
         int min = 0;
         int max = size;
         for(int i = 0; i < size; i++){
             data.push_back(min + (rand() % (max - min + 1)));
         }
         return data;   
+    }
+    else if (type == "reverse"){
+        for (int i = size; i > 0; i--){
+            data.push_back(i);
+        }
+    }
+    else if (type == "sorted"){
+        for (int i = 0; i < size; i++){
+            data.push_back(i);
+        }
     }
 }
